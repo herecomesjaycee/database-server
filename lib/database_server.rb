fc15e635077a099b5da490f06e6db18d116871e2
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-03-06 16:36:22 +0000

    Amend formatting against Rubocop style convention

--- lib/database_server.rb
+++ lib/database_server.rb
@@ -5,19 +5,18 @@ class DatabaseServer < Sinatra::Base
   DATA = {}
   enable :sessions
 
-   get '/' do
-  	  'Hello DatabaseServer!'
-   end
+  get '/' do
+    'Hello DatabaseServer!'
+  end
 
-   get '/set' do
-     params.each{|key,value| DATA.merge!({key => value})}
-    end
-  		
-   get '/get' do
-  	  content_type :json
-  	  DATA[params[:key]].to_json
-   end
-  # start the server if ruby file executed directly
-   run! if app_file == $DatabaseServer
+  get '/set' do
+    params.each { |key, value| DATA.merge!(key => value) }
+  end
 
+  get '/get' do
+    content_type :json
+    DATA[params[:key]].to_json
+  end
+
+  run! if app_file == $DatabaseServer
 end
