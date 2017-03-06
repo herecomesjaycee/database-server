2ba1cc82a98627d47eb9a6872a13d7947b20003e
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-03-06 15:51:23 +0000

    Both set and get method test once

--- lib/database_server.rb
+++ lib/database_server.rb
@@ -1,10 +1,23 @@
 require 'sinatra/base'
+require 'json'
 
 class DatabaseServer < Sinatra::Base
-  get '/' do
-    'Hello DatabaseServer!'
-  end
+	DATA = {}
+	enable :sessions
 
+	get '/' do
+		'Hello DatabaseServer!'
+	end
+
+	get '/set' do
+		params.each{|key,value| DATA.merge!({key => value})}
+	end
+  		
+  	get '/get' do
+  		 content_type :json
+  		 DATA[params[:key]].to_json
+  	end
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
--- spec/features/database_feature_spec.rb
+++ spec/features/database_feature_spec.rb
@@ -8,6 +8,20 @@ feature 'App deployment' do
 		end
 	end
 
+	context 'server can post' do
+		scenario 'server to save session value' do
+			visit('/set?a=1')
+		end
+	end
+
+	context 'server can post' do
+		scenario 'server to save session value' do
+			visit('/set?a=1')
+			visit('/set?b=2')
+			visit('/get?key=a')
+			expect(page).to have_content('1')
+		end
+	end
 end
 
 
